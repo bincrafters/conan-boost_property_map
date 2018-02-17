a5a65ebacbf1e7bc948fe67072ac77a821e8cdb3
Jerry W <jerrywiltse@gmail.com>
2018-02-17 09:39:42 -0500

    merge from testing

--- conanfile.py
+++ conanfile.py
@@ -18,8 +18,8 @@ class BoostProperty_MapConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing",
-        "boost_level14group/1.65.1@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/stable",
+        "boost_level14group/1.65.1@bincrafters/stable"
     )
 
     # BEGIN
@@ -27,7 +27,7 @@ class BoostProperty_MapConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.65.1@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
