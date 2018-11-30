42c3ef272b3592338790fb946a250a664e01eda6
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,16 +4,12 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostProperty_MapConan(base.BoostBaseConan):
     name = "boost_property_map"
     url = "https://github.com/bincrafters/conan-boost_property_map"
     lib_short_names = ["property_map"]
     header_only_libs = ["property_map"]
-    cycle_group = "boost_level14group"
-    b2_requires = [
-        "boost_level14group",
-    ]
-
-
+    cycle_group = "boost_cycle_group_d"
+    b2_requires = ["boost_cycle_group_d"]
